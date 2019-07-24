838da97ee831988c72254d87a65f7d486a674187
Carlos A.L.M. Guimaraes <carguimaraesgma@gmail.com>
2019-07-23 21:32:48 -0300

    gerando nome

--- src/main/java/digicast/filenamegeneretor/Principal.java
+++ src/main/java/digicast/filenamegeneretor/Principal.java
@@ -1,9 +1,12 @@
 package digicast.filenamegeneretor;
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 
+import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -38,11 +41,45 @@ public class Principal implements CommandLineRunner {
 	          }
 	          else
 	          {
-	        	  System.out.println(x.normalize());  
+	        	  copiarARquivo(x.normalize());
+	        	  
 	          }
 			  
 		  });
 		  
+	}
+	
+	private void copiarARquivo(Path path) {
+		
+		//String valor=path.toUri().getPath();
+		
+		String fullPath=path.toUri().getPath();
+		String nomeArquivo=path.getFileName().toString();
+		
+		String novoNome="";
+		String[] filePaths = fullPath.split(File.separator);
+		for(String x : filePaths) {
+			 
+			novoNome=novoNome+"_"+x;
+		}
+		
+				
+		System.out.println(fullPath);  
+	    System.out.println(nomeArquivo);  
+	    System.out.println(novoNome);  
+	    
+		
+	
+		File source = new File(fullPath );
+		File dest = new File("/home/carguimaraes/Documentos/out/"+novoNome);
+		
+		try {
+		    FileUtils.copyFile(source, dest);
+		} catch (IOException e) {
+		    e.printStackTrace();
+		}
+		
+		
 	}
 
 }
